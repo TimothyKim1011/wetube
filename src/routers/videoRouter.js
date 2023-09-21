a10b2ad1e479774e91d39e8fa6c0e9daa47dde81
TimothyKim1011 <138592141+TimothyKim1011@users.noreply.github.com>
2023-09-21 17:48:29 +0900

    0921

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,5 +7,4 @@ videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", see);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-
 export default videoRouter;

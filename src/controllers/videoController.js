1fb0d065a79e9cdc5e36ea64b4c13eb2946d5a3b
TimothyKim1011 <138592141+TimothyKim1011@users.noreply.github.com>
2023-09-24 00:50:03 +0900

    0923

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
     rating: 5,
     comments: 2,
     createdAt: "2 minutes ago",
-    views: 59,
+    views: 1,
     id: 1,
   },
   {
@@ -32,11 +32,24 @@ export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
   return res.render("watch", {
-    pageTitle: `Watching ${video.title}`,
+    pageTitle: `Watching: ${video.title}`,
     video,
   });
 };
-export const edit = (req, res) => res.render("edit");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+
+  return res.redirect(`/videos/${id}`);
+};
+
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -1,22 +1,24 @@
 import express from "express";
 import morgan from "morgan";
-import global from "./routers/globalRouter";
-import user from "./routers/userRouter";
-import video from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
+
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true })); // translate se6 -> javascript object that we can use.
 
 // 누군가 app.use 링크에 접근한다면, 라우터로 안내되어지고, 링크를 get하게 된다.
 // .use는 중간경로, .get은 최종경로
-app.use("/", global);
-app.use("/users", user);
-app.use("/videos", video);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}🚀`);
